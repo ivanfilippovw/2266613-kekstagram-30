cc441d2d0663ded117f3a6a133b069d97856d8c5
Ivan <ivanfilippovw@gmail.com>
2023-10-22 23:17:19 +0400

    feat: update getRandomInteger func

--- js/main.js
+++ js/main.js
@@ -29,10 +29,11 @@ const NAMES =
 [ 'Иван', 'Мария', 'Александр', 'Екатерина', 'Дмитрий', 'Анна', 'Петр', 'София', 'Алексей', 'Ольга', 'Константин', 'Наталья', 'Сергей', 'Виктория', 'Андрей', 'Елена', 'Михаил', 'Татьяна', 'Григорий', 'Людмила', 'Артем', 'Ирина', 'Николай', 'Маргарита', 'Василий', 'Евгения', 'Антон', 'Лариса', 'Вадим', 'Юлия'
 ];
 
-function getRandomInteger (a, b) {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 }
 
@@ -41,14 +42,17 @@ function createRandomNumber (min, max) {
 
   return function () {
     let currentValue = getRandomInteger(min, max);
+
     if (previousValues.length >= (max - min + 1)) {
-      // console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
       return null;
     }
+
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
     }
+
     previousValues.push(currentValue);
+
     return currentValue;
   };
 }
