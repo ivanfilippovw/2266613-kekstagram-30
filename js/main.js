dd04e94f3ae6321eb9ad86519062e03538e36d5b
Ivan <ivanfilippovw@gmail.com>
2023-10-31 23:35:35 +0400

    feat: add renderingThumbnails module

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-import {getPhotosDescription} from '../js/data';
-
-getPhotosDescription();
-
-// eslint-disable-next-line no-console
-console.dir(getPhotosDescription, { depth: null });
-
-// eslint-disable-next-line no-console
-console.log(getPhotosDescription());
+import '../js/renderingThumbnails';
---
+++ js/renderingThumbnails.js
@@ -0,0 +1,25 @@
+import {getPhotosDescription} from '../js/data';
+
+// Находим элемент, куда будем добавлять изображения
+const picturesList = document.querySelector('.pictures');
+
+// Находим фрагмент с содержимым темплейта и в фрагменте находим нужный элемент
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const similarPictures = getPhotosDescription();
+
+const fragment = document.createDocumentFragment();
+
+similarPictures.forEach(({url, description, comments, likes}) => {
+  const newPicture = pictureTemplate.cloneNode(true);
+  newPicture.querySelector('.picture__img').src = url;
+  newPicture.querySelector('.picture__img').alt = description;
+  newPicture.querySelector('.picture__comments').textContent = comments;
+  newPicture.querySelector('.picture__likes').textContent = likes;
+
+  fragment.append(newPicture);
+});
+
+picturesList.append(fragment);
+
+export {};
