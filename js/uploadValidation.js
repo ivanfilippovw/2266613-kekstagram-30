2e2e20dfc111369f6e3512465258d8bfbf21dcb0
Ivan <ivanfilippovw@gmail.com>
2023-11-12 16:31:10 +0400

    refactor: eslint mistakes

--- js/uploadValidation.js
+++ js/uploadValidation.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 import { isEscapeKey } from './util.js';
 
 // Находим элемент-контейнер, где находится форма редактирования изображения
