29dc73ac6c63269b33daf8c489ce272cb2adc676
Ivan <ivanfilippovw@gmail.com>
2023-11-13 18:07:53 +0400

    feat: add module uploadPictureEdit, part 1: edit the image scale

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getPicturesData } from '../js/data.js'; // Функция для получения случайных данных
 import { renderGallery } from './renderGallery.js'; // Функция для создания галереи
 import './uploadPictureForm.js'; // Модуль для работы формы загрузки изображения
+import './uploadPictureEdit.js';
 
 const arrayDataPictures = getPicturesData();
 
